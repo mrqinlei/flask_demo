d7a2e36dd325e7df6be757cfdd663842c103eefe
QinLei <mrqinleii@gmail.com>
2019-12-09 11:06:58 +0800

    debug model on

--- demo.py
+++ demo.py
@@ -2,12 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
 
+@app.route("/")
 def hello():
     return "Hello World!"
 
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, DEBUG=True)
